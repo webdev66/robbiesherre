0bff89fbf90fc99f32bbce942eb2716b311f5f48
Robbie Sherre <robbiesherre@idesigntek.com>
2019-09-16 12:58:45 -0700

    REACT JS sample code

--- react-js/react.js
+++ react-js/react.js
@@ -3,3 +3,41 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(<h1>Hello world</h1>, document.getElementById('app'));
+                
+// Embed expressions in JSX
+const name = 'Robbie Sherre';
+const element = <h1>Hello, {name}</h1>;
+
+ReactDOM.render(
+  element,
+  document.getElementById('root')
+);                
+
+// REACT JS
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Test extends React.Component {
+  render() {
+    return <h1>Hello World!</h1>;
+  }
+}
+
+ReactDOM.render(<Test />, document.getElementById('root'));
+
+
+//REACT HTML
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport"
+      content="width=device-width, initial-scale=1" />
+    <title>React App</title>
+  </head>
+  <body>
+
+    <div id="root"></div>
+
+  </body>
+</html>
